RaineGit/chloebot

commit 95df44bc24c7faf3e2f68da6a97c836bce7707fa

Added placeholders for notes

userFunctions.js
@@ -17,6 +17,31 @@ async function onMessage(msg){
 						var user = await client.users.fetch(userId);
 						if(!user)
 							continue;
+						if(note.includes("{")){
+							note = note.replace(/{.+}/g, v => {
+								switch(v.slice(1, -1).toLowerCase()){
+									case "user_name":
+									case "username":
+										return msg.author.username;
+									case "user_tag":
+										return msg.author.tag;
+									case "user_id":
+										return msg.author.id;
+									case "channel_name":
+										return msg.channel.name;
+									case "channel_id":
+										return msg.channel.id;
+									case "server_name":
+									case "guild_name":
+										return msg.guild ? msg.guild.name : "DM";
+									case "server_id":
+									case "guild_id":
+										return msg.guild ? msg.guild.id : "0";
+									default:
+										return v;
+								}
+							})
+						}
 						await (new Answer(user.username + "'s note is\n> " + note)).send(msg.channel);
 					}
 				}

commit febf704ef3c68fe4125b6eb2c3f639edab958217

Fix bug introduced in commit 95df44bc24c7faf3e2f68da6a97c836bce7707fa

userFunctions.js
@@ -18,7 +18,7 @@ async function onMessage(msg){
 						if(!user)
 							continue;
 						if(note.includes("{")){
-							note = note.replace(/{.+}/g, v => {
+							note = note.replace(/{.+?}/g, v => {
 								switch(v.slice(1, -1).toLowerCase()){
 									case "user_name":
 									case "username":
@@ -40,7 +40,7 @@ async function onMessage(msg){
 									default:
 										return v;
 								}
-							})
+							});
 						}
 						await (new Answer(user.username + "'s note is\n> " + note)).send(msg.channel);
 					}